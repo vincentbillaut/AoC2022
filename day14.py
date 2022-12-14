37baf691e379a743e8e472c7bce6cafdb723a1f0
vincentbillaut <vct.billaut@gmail.com>
2022-12-14 12:27:59 -0500

    fix day 14

--- day14.py
+++ day14.py
@@ -1,13 +1,4 @@
-import math
-import networkx as nx
 import numpy as np
-import re
-
-from collections import defaultdict
-from copy import deepcopy
-from itertools import product
-from tqdm import tqdm_notebook
-
 from start_day import AdventOfCodeDay
 
 
@@ -38,13 +29,13 @@ def move_sand_down(x, y, occupied_dict, max_depth, is_floor):
 
 
 def sand_falling(source, init_occupied_dict, floor_allowed):
-    occupied_dict, count, max_depth = init_occupied_dict.copy(), 0
+    occupied_dict, count = init_occupied_dict.copy(), 0
     max_depth = max(y for _, y in occupied_dict) + 2 * int(floor_allowed)
-    at_rest, pos = move_sand_down_test(source[0], source[1], occupied_dict, max_depth, floor_allowed)
+    at_rest, pos = move_sand_down(source[0], source[1], occupied_dict, max_depth, floor_allowed)
     while at_rest and source not in occupied_dict:
         occupied_dict.add(pos)
         count += 1
-        at_rest, pos = move_sand_down_test(source[0], source[1], occupied_dict, max_depth, floor_allowed)
+        at_rest, pos = move_sand_down(source[0], source[1], occupied_dict, max_depth, floor_allowed)
     return count
 
 
